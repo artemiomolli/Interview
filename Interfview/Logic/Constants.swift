e124755d6385e8b5ff21f96c20acd06c25647d19
Артём Гуральник <artem.guralnyk@skysoft.tech>
2018-11-24 16:48:48 +0200

    Segue identifiers created for constants

---
+++ Interfview/Logic/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Interfview
+//
+//  Created by Артём Гуральник on 11/24/18.
+//  Copyright © 2018 Артём Гуральник. All rights reserved.
+//
+
+import Foundation
+
+struct Segues {
+    
+    static let startToMainMenu = "selectToMain"
+    static let signInToMain = "signInToMain"
+}
+
+
